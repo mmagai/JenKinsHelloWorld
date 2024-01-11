b1a0e16eaddbf4769a1b67be04154077799feb86
mmagai <54861459+mmagai@users.noreply.github.com>
2024-01-10 17:53:19 -0700

    Update PostController.java

--- PostController.java
+++ PostController.java
@@ -7,6 +7,6 @@ public class PostController {
 
     public static void main(String args[]){
         
-        System.out.println("Trying,welcome to JEnkins");
+        System.out.println("Trying, to JEnkins");
     }
 }
